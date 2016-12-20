9831c45842ae3e081f610de76d525a07d3e6139d
zinbers <359565935@qq.com>
2016-12-20 23:54:21 +0800

    count words

---
+++ countWord.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+#对一个txt文件进行单词统计，并按从高到底的顺序排列输出到out.txt中
+awk -F'[,. ]' '{for(i=1;i<=NF;i++)a[$i]++} END{for(j in a)print j,"\t",a[j]}' /d/code_test/countWord/test.txt | sort -nrk 2 >>/d/code_test/countWord/out.txt
+
+
